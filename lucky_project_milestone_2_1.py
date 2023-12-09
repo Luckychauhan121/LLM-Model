722b438d66d61dd1a80461c2136b78012ae6ce17
Lucky Chauhan <122059672+Luckychauhan121@users.noreply.github.com>
2023-12-08 20:23:52 -0500

    GPTlegal

--- lucky_project_milestone_2_1.py
+++ lucky_project_milestone_2_1.py
@@ -7,6 +7,25 @@ Original file is located at
     https://colab.research.google.com/drive/1NEh2pO_b8XxK3wbUSF-8Rrbz4QVg6yK-
 """
 
+import streamlit as st
+import openai
+import os
+# Set up your OpenAI API key
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+# Initialize Streamlit
+st.title("Legal Alexi")
+# Create a text input field for user queries
+user_input = st.text_input("Ask a question:")
+# Send the user's query to OpenAI GPT-3
+if user_input:
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=user_input,
+    max_tokens=50
+    )
+    st.write(response['choices'][0]['text'].strip())
+
+
 ! pip install streamlit -q
 
 !pip install langchain -q
